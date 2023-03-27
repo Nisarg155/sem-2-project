4b54b71227b20f47140fd1a314d05d9fe1eede9a
nisarg_amlani_ubuntu <amlaninisarg@gmail.com>
2023-03-27 09:33:08 +0530

    modified funsection to show all the piece in a box

--- jigsawgame.js
+++ jigsawgame.js
@@ -1,41 +1,34 @@
-console.log('This is jigsaw game code');
-const bbox = document.getElementById('bigbox');
-const box1 = document.querySelector('.smallbox1'); 
-const box2 = document.querySelector('.smallbox2');
-const box3 = document.querySelector('.smallbox3');
-const box4 = document.querySelector('.smallbox4');
+var rows = 2;
+var col = 2;
 
-const img1 = document.querySelector('piece1');
-const img2 = document.querySelector('piece2');
-const img3 = document.querySelector('piece3');
-const img4 = document.querySelector('piece4');
-
-//? eventlistner for draggable element img1
-img1.addEventListener('dragstart',()=>{
-    //? when drag starts
-    console.log('Dragstart has been triggered');
-})
-img1.addEventListener('dragend',()=>{
-    //? when drag ends
-    console.log('Dragend had been trigerred ');
-})
-
-box1.addEventListener('dragover',()=>{
+var currtile;
+var othertile;
 
 
-})
-box1.addEventListener('dragenter',()=>{
+window.onload = function () {
 
+    //? initializing 2*2 board
+    for(let r = 0 ; r<rows ; r++)
+    {
+        for(let c= 0 ;c<col;c++)
+        {
+            let tile = document.createElement("img");
+            tile.src = "whiteimg.png";
 
-})
-box1.addEventListener('dragleave',()=>{
-
-
-})
-box1.addEventListener('dragenter',()=>{
-
-
-})
-
+            document.getElementById("bigbox").append(tile);
+        }
+    }
+    //? piece
+    let piece = [];
+    for(let i =1 ;i<=r*c;i++)
+    {
+        piece.push(i.toString());
+    }
+    for(let i = 0;i<piece.length;i++)
+    {
+        
+    }
+    
+}
 
 
